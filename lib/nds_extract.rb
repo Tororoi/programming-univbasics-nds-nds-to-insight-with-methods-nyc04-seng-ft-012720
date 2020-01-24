e7c697d043af1923c3b123281308b97d00aa4f48
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-24 20:28:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,12 @@ def gross_for_director(director_data)
   total = 0
   index = 0
 
-  while index < director_data[:movies][index][:worldwide_gross]
+  while index < director_data[:movies].length do
+    total += director_data[:movies][index][:worldwide_gross]
+    index += 1
+  end
+
+  total
 end
 
 # Write a method that, given an NDS creates a new Hash
